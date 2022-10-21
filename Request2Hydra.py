ac655878a778b5b7e1ea9e8e7aefb22b269bff9b
Thetvdh <tvdjthe@protonmail.com>
2022-10-21 20:39:43 +0100

    Updated code comments.

--- Request2Hydra.py
+++ Request2Hydra.py
@@ -5,13 +5,17 @@ from RequestHandler import RequestHandler
 
 class Request2Hydra:
 
+    """
+    # __init__()
+    # Sets up all variables to use in this class.
+    """
     def __init__(self):
         self.args = self.get_cmd_args()
-        self.request_handler = RequestHandler(self.args.rfile)
+        self.request_handler = RequestHandler(self.args.rfile)  # Creates the request handler object
         self.username = self.args.username
         self.password = self.args.password
-        self.host = f"{self.request_handler.host}"
-        self.port = f"{self.request_handler.port}"
+        self.host = self.request_handler.host
+        self.port = self.request_handler.port
         self.endpoint = self.request_handler.endpoint
         self.post_data = self.request_handler.post_data.keys()
         self.parsed_post_data = self.parse_post_data()
@@ -38,6 +42,13 @@ class Request2Hydra:
 
         return parser.parse_args()
 
+    """
+    # parse_flags()
+    # Checks if the username and password supplied are directories or files. 
+    # Sets the correct flag accordingly
+    # Loops through any other flags in the array and adds them to a string version of the array
+    # This is then added to the end of the command
+    """
     def parse_flags(self):
         if os.path.exists(self.args.username):
             self.flags.append("-L")
@@ -53,6 +64,12 @@ class Request2Hydra:
         for i in range(2, len(self.flags)):
             self.string_flags = "".join(self.flags[i])
 
+    """
+    # parse_post_data()
+    # Parses the post data gathered from the RequestHandler()
+    # checks to see if the post data is a username or password and if it is applies the special hydra syntax
+    # Otherwise it just adds it to the end
+    """
     def parse_post_data(self):
         output = ""
         for item in self.post_data:
@@ -64,6 +81,6 @@ class Request2Hydra:
                 output += item
             output += "&"
 
-        if output[-1] == '&':
+        if output[-1] == '&':  # Removes trailing ampersand leftover from the for loop
             output = output[:-1]
         return output
--- RequestHandler.py
+++ RequestHandler.py
@@ -5,6 +5,10 @@ import re
 
 
 class RequestHandler:
+    """
+    # __init__()
+    # Sets up the required variables for use in the program. setup() populates all these variables
+    """
 
     def __init__(self, request_file):
         self.request_file = request_file
@@ -16,11 +20,28 @@ class RequestHandler:
         self.url = None
         self.setup()
 
+    """
+    # extract_post_data()
+    # takes the last line from a burp suite request. This contains the post data. 
+    # In current state program assumes the file is a POST request.
+    # Places the post data into a dictionary, splitting by the "&" sign to determine each key:value pair and the = sign
+    # To determine what is the key and what is the value
+    """
+
     def extract_post_data(self):
         post_data = self.request_lines[-1]
         post_data = post_data.split("&")
         for data in post_data:
             self.post_data[data.split("=")[0]] = data.split("=")[1]
+
+    """
+    # extract_host()
+    # Uses regex to extract the host and port, if supplied, from the request. Looks at the second line in the request
+    # which looks like
+    # Host: {host here}
+    # Splits by the ':' character
+    # Tries to enter them into variables but if the list index is out of range then the port is set to None
+    """
 
     def extract_host(self):
         host = self.request_lines[1]
@@ -30,16 +51,22 @@ class RequestHandler:
         try:
             self.host = host[0]
             self.port = host[1]
-        except Exception:
+        except IndexError:
             self.host = host[0]
             self.port = None
-
-
-
+    """
+    # get_request_lines()
+    # Simple function to split the request file into individual lines for manipulation. Stored in a list
+    """
     def get_request_lines(self):
         with open(self.request_file, "r") as request_file:
             self.request_lines = request_file.read().splitlines()
 
+    """
+    # setup()
+    # called in __init__()
+    # Runs all the necessary methods to populate the init variables.
+    """
     def setup(self):
         self.get_request_lines()
         self.extract_post_data()
@@ -50,6 +77,9 @@ class RequestHandler:
             self.url += f":{str(self.port)}"
         self.url += self.endpoint
 
+    """
+    Debugging overloaded __str__(). Never hit when run from main.py
+    """
     def __str__(self):
         data = f"\nRequest File: {self.request_file}\n"
         data += "\nRequest Data: "
@@ -61,22 +91,35 @@ class RequestHandler:
         data += f"\nUrl: {self.url}\n"
         return data
 
+    """
+    # extract_endpoint()
+    # looks at the first line in the request, should look something like this.
+    # POST /endpoint HTTP/1.1
+    # Extracts just the endpoint for use in the URL. 
+    """
     def extract_endpoint(self):
         endpoint = self.request_lines[0]
         endpoint = endpoint.split(" ")
         self.endpoint = endpoint[1]
 
+    """
+    # Attempts to automatically get the failure message from the site. 
+    # If it is unable to connect for some reason then an option is given to manually enter the failure string
+    # 
+    """
     def get_failure_message(self):
         try:
-            get_resp = requests.get(self.url)
-            post_resp = requests.post(self.url, self.post_data)
-            get_resp_list = get_resp.text.splitlines()
-            post_resp_list = post_resp.text.splitlines()
+            get_resp = requests.get(self.url)  # Sends get request
+            post_resp = requests.post(self.url, self.post_data)  # Sends post request
+            get_resp_list = get_resp.text.splitlines()  # splits the get request into individual lines
+            post_resp_list = post_resp.text.splitlines()  # splits the post request into individual lines
 
+            # Compares the difference between the two requests. Extracts the difference.
             difference = list(set(get_resp_list) ^ set(post_resp_list))[0]
-            difference = re.sub("<.*?>", "", difference.lstrip())
+            difference = re.sub("<.*?>", "", difference.lstrip())  # Removes any leading chars and any HTML tags
+            difference = difference.rstrip()  # Removes any trailing chars
             return difference
-        except requests.exceptions.ConnectionError as err:
+        except requests.exceptions.ConnectionError:
             choice = input("Unable to automatically detect fail string. Supply string manually? Y/N: ").upper()
             if choice == 'Y':
                 return input("Enter the string: ")
@@ -84,7 +127,3 @@ class RequestHandler:
                 print("Unable to complete program. Exiting...")
                 sys.exit()
 
-if __name__ == '__main__':
-    handler = RequestHandler("request.txt")
-    print(handler)
-    handler.get_failure_message()
